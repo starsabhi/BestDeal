15654c598ecd52444d5112948e424ab85c741052
abhishek <starsabhishek@gmail.com>
2022-06-07 23:46:00 -0500

    Bug in delployment

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,12 +4,6 @@ const router = express.Router();
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
-const { restoreUser } = require('../utils/auth');
-
-router.get('/hello/world', function (req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
 
 // Static routes
 // Serve React build files in production
@@ -18,7 +12,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
@@ -29,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
@@ -39,8 +33,10 @@ if (process.env.NODE_ENV === 'production') {
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
+    res.status(201).json({});
   });
 }
+
+module.exports = router;
 
 module.exports = router;
