43b85d61c1475bac3c1fc5e031cf6f065be9af63
abhishek <starsabhishek@gmail.com>
2022-06-13 01:41:40 -0500

    Cart Total price

--- frontend/src/components/Cart/CartDetailPage/index.js
+++ frontend/src/components/Cart/CartDetailPage/index.js
@@ -4,6 +4,7 @@ import { getCarts } from '../../../store/cart';
 import { getAllProduct } from '../../../store/product';
 import './CartDetailPage.css';
 import ClipLoader from 'react-spinners/ClipLoader';
+import TotalPriceCart from '../TotalPriceCart';
 
 function CartDetailPage() {
   const sessionUser = useSelector((state) => state.session.user);
@@ -33,13 +34,23 @@ function CartDetailPage() {
           </div>
         ) : (
           <div>
-            {cart?.map((item) => (
-              <div key={item?.id}>
-                <div>{item?.name}</div>
-                <img src={item?.imageUrl} alt={item?.imageUrl}></img>
-                <div>{item.price}</div>
-              </div>
-            ))}
+            <div>
+              <TotalPriceCart />
+            </div>
+            <div className="CartDeatailPageMainDiv">
+              {cart?.map((item) => (
+                <div className="CartDeatailPageInnerDiv" key={item?.id}>
+                  <div>{item?.name}</div>
+                  <img
+                    className="imageforCartdetailPage"
+                    src={item?.imageUrl}
+                    alt={item?.imageUrl}
+                  ></img>
+                  <div>${item.price}</div>
+                </div>
+              ))}
+              <div>Price</div>
+            </div>
           </div>
         )}
       </div>
---
+++ frontend/src/components/Cart/TotalPriceCart/index.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCarts } from '../../../store/cart';
+
+export default function TotalPriceCart() {
+  const sessionUser = useSelector((state) => state.session.user);
+  const dispatch = useDispatch();
+  const Cart = useSelector((state) => state.cart);
+  const ItemArr = Object.values(Cart);
+  // console.log(ItemArr);
+  // const ItemArr = [0, 1, 2, 3];
+
+  useEffect(() => {
+    dispatch(getCarts(sessionUser?.id));
+  }, [dispatch]);
+  const initialValue = 0;
+  const theSum = ItemArr.reduce(function (accumulator, currentValue) {
+    return (
+      accumulator +
+      parseFloat(currentValue.price) * parseFloat(currentValue.quantity)
+    );
+  }, initialValue);
+  console.log(theSum);
+
+  console.log('How many time value changes', theSum);
+  return (
+    <>
+      <div>
+        <div className="totalPriceNumber">TOTAL PRICE ${theSum}</div>
+      </div>
+    </>
+  );
+}
