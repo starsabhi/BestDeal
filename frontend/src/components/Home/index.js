3da3ee6e6f52c17e781e6f83e34d488ec2164150
abhishek <starsabhishek@gmail.com>
2022-07-07 11:35:45 -0500

    Home page in Progress

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,18 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllProduct } from '../../store/product';
-import Productsdetail from '../Productsdetail';
 import './Home.css';
-import Cash from '../../images/cash.svg';
 
 function Home() {
   const dispatch = useDispatch();
-  const productList = useSelector((state) => Object.values(state.product));
-  // console.log(productList, '***************');
+
   useEffect(() => {
     dispatch(getAllProduct());
   }, [dispatch]);
+
+  let productList = useSelector((state) => Object.values(state.product));
+  // console.log(productList, '***************');
+  let [newArr, setNewArr] = useState([]);
+  // console.log(catogory);
+  const [allProducts, setAllProducts] = useState(true);
+  const newFunction = (catoString) => {
+    setAllProducts(false);
+    setNewArr(productList.filter((product) => product.category === catoString));
+    // console.log(newArr);
+  };
 
   return (
     <>
@@ -20,44 +28,78 @@ function Home() {
         <h2 className="productlistnameclass">
           <span>Our Product List</span>
         </h2>
+        <button onClick={() => setAllProducts(true)}>All</button>
+        <button onClick={() => newFunction('Men')}>Men</button>
+        <button onClick={() => newFunction('Kids')}>kids</button>
         <div className="HomepageMainDiv">
           <div className="productListClass">
-            {productList?.map(
-              ({
-                id,
-                name,
-                price,
-                imageUrl,
-                description,
-                productInfo,
-                category,
-              }) => (
-                <div key={id} className="productClassArr">
-                  <div className="contentdivforHome">
-                    <NavLink
-                      style={{ color: 'inherit', textDecoration: 'inherit' }}
-                      to={`products/${id}`}
-                    >
-                      <div className="imageListDivevery">
-                        <img className="productListimage" src={imageUrl} />
+            {allProducts
+              ? productList?.map(
+                  ({
+                    id,
+                    name,
+                    price,
+                    imageUrl,
+                    description,
+                    productInfo,
+                    category,
+                  }) => (
+                    <div key={id} className="productClassArr">
+                      <div className="contentdivforHome">
+                        <NavLink
+                          style={{
+                            color: 'inherit',
+                            textDecoration: 'inherit',
+                          }}
+                          to={`products/${id}`}
+                        >
+                          <div className="imageListDivevery">
+                            <img className="productListimage" src={imageUrl} />
+                          </div>
+                          <div className="productnameDivforMain">
+                            <div className="productNameh2">{name}</div>
+                          </div>
+                          <div className="priceMainDiv">
+                            <div className="mainPriceDiv">${price}</div>
+                          </div>
+                        </NavLink>
                       </div>
-                      <div className="productnameDivforMain">
-                        <div className="productNameh2">{name}</div>
+                    </div>
+                  )
+                )
+              : newArr?.map(
+                  ({
+                    id,
+                    name,
+                    price,
+                    imageUrl,
+                    description,
+                    productInfo,
+                    category,
+                  }) => (
+                    <div key={id} className="productClassArr">
+                      <div className="contentdivforHome">
+                        <NavLink
+                          style={{
+                            color: 'inherit',
+                            textDecoration: 'inherit',
+                          }}
+                          to={`products/${id}`}
+                        >
+                          <div className="imageListDivevery">
+                            <img className="productListimage" src={imageUrl} />
+                          </div>
+                          <div className="productnameDivforMain">
+                            <div className="productNameh2">{name}</div>
+                          </div>
+                          <div className="priceMainDiv">
+                            <div className="mainPriceDiv">${price}</div>
+                          </div>
+                        </NavLink>
                       </div>
-                      <div className="priceMainDiv">
-                        <div className="mainPriceDiv">
-                          <span class="material-symbols-outlined">
-                            attach_money
-                          </span>
-                          {price}
-                        </div>
-                      </div>
-                    </NavLink>
-                    {/* <p className="descriptionptag">{description}</p> */}
-                  </div>
-                </div>
-              )
-            )}
+                    </div>
+                  )
+                )}
           </div>
         </div>
       </div>
